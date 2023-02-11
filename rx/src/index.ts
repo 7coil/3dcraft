16af53a75e4de717daea9d248a77494f36b09a11
SojaMilch <hlrockwelljones@gmail.com>
2023-02-11 17:50:45 +0000

    added moving ability + read/transmit array

--- rx/src/index.ts
+++ rx/src/index.ts
@@ -1,7 +1,36 @@
 rednet.open("right");
-
 const [id, msg] = rednet.receive();
+rednet.close("right");
 
-if (msg == "start") {
-  print("hello");
+let blocks: number[][] = [];
+const layers = 2;
+
+print("generating array..");
+
+for (let y = 0; y < 32; y++) {
+  blocks[y] = [];
+  for (let x = 0; x < 32; x++) {
+    blocks[y][x] = 0;
+  }
 }
+
+for (let y = 0; y < layers; y++) {
+  for (let x = 0; x < 32; x++) {
+    const [block, reason] = turtle.dig();
+    turtle.forward();
+    if (block == true) {
+      print("found Block");
+      blocks[y][x] = 1;
+    }
+  }
+  turtle.forward();
+  turtle.up();
+  turtle.turnLeft();
+  turtle.turnLeft();
+}
+
+print(blocks);
+
+rednet.open("right");
+rednet.send(4, blocks);
+rednet.close("right");
