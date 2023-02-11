8e7892b3202cb69b05e899544aeb64c250f2a0b3
Leondro Lio <github@leondrolio.com>
2023-02-11 12:46:34 +0000

    setup

---
+++ tx/dist/startup.lua
@@ -0,0 +1,28 @@
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+
+local ____modules = {}
+local ____moduleCache = {}
+local ____originalRequire = require
+local function require(file, ...)
+    if ____moduleCache[file] then
+        return ____moduleCache[file].value
+    end
+    if ____modules[file] then
+        local module = ____modules[file]
+        ____moduleCache[file] = { value = (select("#", ...) > 0) and module(...) or module(file) }
+        return ____moduleCache[file].value
+    else
+        if ____originalRequire then
+            return ____originalRequire(file)
+        else
+            error("module '" .. file .. "' not found")
+        end
+    end
+end
+____modules = {
+["index"] = function(...) 
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+print("Hello")
+ end,
+}
+return require("index", ...)
