f559aec29b91bf4b8f74ae207be1f3b418e79319
Harrison Ndugba <harrison.november@icloud.com>
2023-02-11 18:40:05 +0000

    Modifed Turtle Move

--- tx/src/TurtleMove.ts
+++ tx/src/TurtleMove.ts
@@ -1,11 +1,6 @@
 // createBlocks: generates a 3D matrix of blocks.
 // Path: tx/src/createBlocks.ts
 
-export default function TurtleMove() {
-	rednet.open("right");
-	rednet.broadcast("start");
-	turtle.forward();
-	rednet.close("right");
-}
+export default function TurtleMove() {}
 
 print("TurtleMove() has executed.");
